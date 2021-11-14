fabf9f3bc161f88868320412a64ec8b69644bcd8
Florian Maunier <fmauneko@dissidence.ovh>
2021-11-14 23:15:07 +0100

    chore: init boilerplate project

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jsdoc/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'jsdoc',
+  ],
+  rules: {
+  },
+};
